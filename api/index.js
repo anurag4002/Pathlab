c19680e5246690ec44ee1baead81fd7e2e5f69b0
anurag4002 <mishraanurag66031@gmail.com>
2026-09-04 13:40:47 +0530

    Export express app directly in api/index.js and add express-rate-limit to root dependencies

--- api/index.js
+++ api/index.js
@@ -1,11 +1,3 @@
 const app = require('../backend/src/app');
-const connectDatabase = require('../backend/src/config/database');
 
-module.exports = async (req, res) => {
-  try {
-    await connectDatabase();
-  } catch (err) {
-    console.error('Database connection error in Vercel handler:', err.message);
-  }
-  return app(req, res);
-};
+module.exports = app;
